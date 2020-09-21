2d7a1ea8a06b35876dd44d867449665946432694
NocturnalProgress <26844071+NocturnalProgress@users.noreply.github.com>
2020-09-20 22:38:41 -0400

    Can now generate QR Codes and nav to different menus

---
+++ Assets/Scripts/ButtonGroupScript.cs
@@ -0,0 +1,220 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonGroupScript : MonoBehaviour
+{
+    public Canvas mainCanvas;
+    public Canvas initialDataCanvas;
+    public Canvas autonomousCanvas;
+    public Canvas teleOpCanvas;
+    public Canvas endGameCanvas;
+    public Canvas submitDataCanvas;
+
+    private CanvasGroup mainCanvasGroup;
+    private CanvasGroup initalDataCanvasGroup;
+    private CanvasGroup autonomousCanvasGroup;
+    private CanvasGroup teleOpCanvasGroup;
+    private CanvasGroup endGameCanvasGroup;
+    private CanvasGroup submitDataCanvasGroup;
+
+    void Awake()
+    {
+        mainCanvasGroup = mainCanvas.GetComponent<CanvasGroup>();
+        initalDataCanvasGroup = initialDataCanvas.GetComponent<CanvasGroup>();
+        autonomousCanvasGroup = autonomousCanvas.GetComponent<CanvasGroup>();
+        teleOpCanvasGroup = teleOpCanvas.GetComponent<CanvasGroup>();
+        endGameCanvasGroup = endGameCanvas.GetComponent<CanvasGroup>();
+        submitDataCanvasGroup = submitDataCanvas.GetComponent<CanvasGroup>();
+    }
+
+    void Start()
+    {
+        mainCanvasGroup.alpha = 1;
+        mainCanvasGroup.interactable = true;
+        mainCanvasGroup.blocksRaycasts = true;
+
+        initalDataCanvasGroup.alpha = 0;
+        initalDataCanvasGroup.interactable = false;
+        initalDataCanvasGroup.blocksRaycasts = false;
+
+        autonomousCanvasGroup.alpha = 0;
+        autonomousCanvasGroup.interactable = false;
+        autonomousCanvasGroup.blocksRaycasts = false;
+
+        teleOpCanvasGroup.alpha = 0;
+        teleOpCanvasGroup.interactable = false;
+        teleOpCanvasGroup.blocksRaycasts = false;
+
+        endGameCanvasGroup.alpha = 0;
+        endGameCanvasGroup.interactable = false;
+        endGameCanvasGroup.blocksRaycasts = false;
+
+        submitDataCanvasGroup.alpha = 0;
+        submitDataCanvasGroup.interactable = false;
+        submitDataCanvasGroup.blocksRaycasts = false;
+    }
+
+    public void MainButton()
+    {
+        mainCanvasGroup.alpha = 1;
+        mainCanvasGroup.interactable = true;
+        mainCanvasGroup.blocksRaycasts = true;
+
+        initalDataCanvasGroup.alpha = 0;
+        initalDataCanvasGroup.interactable = false;
+        initalDataCanvasGroup.blocksRaycasts = false;
+
+        autonomousCanvasGroup.alpha = 0;
+        autonomousCanvasGroup.interactable = false;
+        autonomousCanvasGroup.blocksRaycasts = false;
+
+        teleOpCanvasGroup.alpha = 0;
+        teleOpCanvasGroup.interactable = false;
+        teleOpCanvasGroup.blocksRaycasts = false;
+
+        endGameCanvasGroup.alpha = 0;
+        endGameCanvasGroup.interactable = false;
+        endGameCanvasGroup.blocksRaycasts = false;
+
+        submitDataCanvasGroup.alpha = 0;
+        submitDataCanvasGroup.interactable = false;
+        submitDataCanvasGroup.blocksRaycasts = false;
+    }
+
+    public void InitalDataButton()
+    {
+        mainCanvasGroup.alpha = 0;
+        mainCanvasGroup.interactable = false;
+        mainCanvasGroup.blocksRaycasts = false;
+
+        initalDataCanvasGroup.alpha = 1;
+        initalDataCanvasGroup.interactable = true;
+        initalDataCanvasGroup.blocksRaycasts = true;
+
+        autonomousCanvasGroup.alpha = 0;
+        autonomousCanvasGroup.interactable = false;
+        autonomousCanvasGroup.blocksRaycasts = false;
+
+        teleOpCanvasGroup.alpha = 0;
+        teleOpCanvasGroup.interactable = false;
+        teleOpCanvasGroup.blocksRaycasts = false;
+
+        endGameCanvasGroup.alpha = 0;
+        endGameCanvasGroup.interactable = false;
+        endGameCanvasGroup.blocksRaycasts = false;
+
+        submitDataCanvasGroup.alpha = 0;
+        submitDataCanvasGroup.interactable = false;
+        submitDataCanvasGroup.blocksRaycasts = false;
+    }
+
+    public void AutonomousButton()
+    {
+        mainCanvasGroup.alpha = 0;
+        mainCanvasGroup.interactable = false;
+        mainCanvasGroup.blocksRaycasts = false;
+
+        initalDataCanvasGroup.alpha = 0;
+        initalDataCanvasGroup.interactable = false;
+        initalDataCanvasGroup.blocksRaycasts = false;
+
+        autonomousCanvasGroup.alpha = 1;
+        autonomousCanvasGroup.interactable = true;
+        autonomousCanvasGroup.blocksRaycasts = true;
+
+        teleOpCanvasGroup.alpha = 0;
+        teleOpCanvasGroup.interactable = false;
+        teleOpCanvasGroup.blocksRaycasts = false;
+
+        endGameCanvasGroup.alpha = 0;
+        endGameCanvasGroup.interactable = false;
+        endGameCanvasGroup.blocksRaycasts = false;
+
+        submitDataCanvasGroup.alpha = 0;
+        submitDataCanvasGroup.interactable = false;
+        submitDataCanvasGroup.blocksRaycasts = false;
+    }
+
+    public void TeleOpButton()
+    {
+        mainCanvasGroup.alpha = 0;
+        mainCanvasGroup.interactable = false;
+        mainCanvasGroup.blocksRaycasts = false;
+
+        initalDataCanvasGroup.alpha = 0;
+        initalDataCanvasGroup.interactable = false;
+        initalDataCanvasGroup.blocksRaycasts = false;
+
+        autonomousCanvasGroup.alpha = 0;
+        autonomousCanvasGroup.interactable = false;
+        autonomousCanvasGroup.blocksRaycasts = false;
+
+        teleOpCanvasGroup.alpha = 1;
+        teleOpCanvasGroup.interactable = true;
+        teleOpCanvasGroup.blocksRaycasts = true;
+
+        endGameCanvasGroup.alpha = 0;
+        endGameCanvasGroup.interactable = false;
+        endGameCanvasGroup.blocksRaycasts = false;
+
+        submitDataCanvasGroup.alpha = 0;
+        submitDataCanvasGroup.interactable = false;
+        submitDataCanvasGroup.blocksRaycasts = false;
+    }
+
+    public void EndGameButton()
+    {
+        mainCanvasGroup.alpha = 0;
+        mainCanvasGroup.interactable = false;
+        mainCanvasGroup.blocksRaycasts = false;
+
+        initalDataCanvasGroup.alpha = 0;
+        initalDataCanvasGroup.interactable = false;
+        initalDataCanvasGroup.blocksRaycasts = false;
+
+        autonomousCanvasGroup.alpha = 0;
+        autonomousCanvasGroup.interactable = false;
+        autonomousCanvasGroup.blocksRaycasts = false;
+
+        teleOpCanvasGroup.alpha = 0;
+        teleOpCanvasGroup.interactable = false;
+        teleOpCanvasGroup.blocksRaycasts = false;
+
+        endGameCanvasGroup.alpha = 1;
+        endGameCanvasGroup.interactable = true;
+        endGameCanvasGroup.blocksRaycasts = true;
+
+        submitDataCanvasGroup.alpha = 0;
+        submitDataCanvasGroup.interactable = false;
+        submitDataCanvasGroup.blocksRaycasts = false;
+    }
+
+    public void SubmitDataButton()
+    {
+        mainCanvasGroup.alpha = 0;
+        mainCanvasGroup.interactable = false;
+        mainCanvasGroup.blocksRaycasts = false;
+
+        initalDataCanvasGroup.alpha = 0;
+        initalDataCanvasGroup.interactable = false;
+        initalDataCanvasGroup.blocksRaycasts = false;
+
+        autonomousCanvasGroup.alpha = 0;
+        autonomousCanvasGroup.interactable = false;
+        autonomousCanvasGroup.blocksRaycasts = false;
+
+        teleOpCanvasGroup.alpha = 0;
+        teleOpCanvasGroup.interactable = false;
+        teleOpCanvasGroup.blocksRaycasts = false;
+
+        endGameCanvasGroup.alpha = 0;
+        endGameCanvasGroup.interactable = false;
+        endGameCanvasGroup.blocksRaycasts = false;
+
+        submitDataCanvasGroup.alpha = 1;
+        submitDataCanvasGroup.interactable = true;
+        submitDataCanvasGroup.blocksRaycasts = true;
+    }
+}
---
+++ Assets/Scripts/GenerateQRCode.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using ZXing;
+using ZXing.Common;
+using TMPro;
+using System;
+
+public class GenerateQRCode : MonoBehaviour
+{
+    [SerializeField] private BarcodeFormat format = BarcodeFormat.QR_CODE;
+    private string data = "";
+    private int width;
+    private int height;
+    private RawImage cRawImage;
+
+    public TMP_InputField teamNumberInputField;
+    public TMP_InputField nameInputField;
+
+    // private void Start()
+    // {
+    //     cRawImage = GetComponent<RawImage>();
+    //     // Generate the texture
+    //     Texture2D tex = GenerateBarcode(data, format, width, height);
+    //     // Setup the RawImage
+    //     cRawImage.texture = tex;
+    //     cRawImage.rectTransform.sizeDelta = new Vector2(tex.width, tex.height);
+    // }
+
+    void Start()
+    {
+        cRawImage = GetComponent<RawImage>();
+        width = Convert.ToInt32(cRawImage.rectTransform.rect.width);
+        height = Convert.ToInt32(cRawImage.rectTransform.rect.height);
+    }
+
+    void LateUpdate()
+    {
+
+    }
+
+    public void GenerateQRCodeButton()
+    {
+        data = teamNumberInputField.text + " " + nameInputField.text;
+
+        cRawImage = GetComponent<RawImage>();
+        // Generate the texture
+        Texture2D tex = GenerateBarcode(data, format, width, height);
+        // Setup the RawImage
+        cRawImage.texture = tex;
+        cRawImage.rectTransform.sizeDelta = new Vector2(tex.width, tex.height);
+    }
+
+    private Texture2D GenerateBarcode(string data, BarcodeFormat format, int width, int height)
+    {
+        // Generate the BitMatrix
+        BitMatrix bitMatrix = new MultiFormatWriter()
+            .encode(data, format, width, height);
+        // Generate the pixel array
+        Color[] pixels = new Color[bitMatrix.Width * bitMatrix.Height];
+        int pos = 0;
+        for (int y = 0; y < bitMatrix.Height; y++)
+        {
+            for (int x = 0; x < bitMatrix.Width; x++)
+            {
+                pixels[pos++] = bitMatrix[x, y] ? Color.black : Color.white;
+            }
+        }
+        // Setup the texture
+        Texture2D tex = new Texture2D(bitMatrix.Width, bitMatrix.Height);
+        tex.SetPixels(pixels);
+        tex.Apply();
+        return tex;
+    }
+
+}
