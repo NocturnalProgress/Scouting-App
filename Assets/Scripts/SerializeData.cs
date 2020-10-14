d02e133baeae53ccee506c5d72d4581d5ab273ef
NocturnalProgress <26844071+NocturnalProgress@users.noreply.github.com>
2020-10-14 00:03:32 -0400

    Added random number to end of json files

--- Assets/Scripts/SerializeData.cs
+++ Assets/Scripts/SerializeData.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 using TMPro;
 
 public class SerializeData : MonoBehaviour
@@ -13,21 +14,24 @@ public class SerializeData : MonoBehaviour
 
     void Start()
     {
-#if UNITY_EDITOR
-        jsonPath = Application.persistentDataPath + "/ScoutingData/data.json";
-#elif UNITY_STANDALONE_WIN
-        jsonPath = Application.persistentDataPath + "/ScoutingData/data.json";
-#elif UNITY_IOS
-        jsonPath = Application.persistentDataPath + "/data.json";
-#elif UNITY_STANDALONE_OSX
-        jsonPath = Application.persistentDataPath + "/data.json";
-#endif
-
+        // string timeDate = DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToShortTimeString();
 
     }
 
     public void SaveToJson()
     {
+        int randomNumber = UnityEngine.Random.Range(00000, 99999);
+
+#if UNITY_EDITOR
+        jsonPath = Application.persistentDataPath + "/ScoutingData" + randomNumber + ".json";
+#elif UNITY_STANDALONE_WIN
+        jsonPath = Application.persistentDataPath + "/ScoutingData" + randomNumber + ".json";
+#elif UNITY_IOS
+        jsonPath = Application.persistentDataPath + "/ScoutingData" + randomNumber + ".json";
+#elif UNITY_STANDALONE_OSX
+        jsonPath = Application.persistentDataPath + "/ScoutingData" + randomNumber + ".json";
+#endif
+
         string json = data.GetComponent<Data>().SerializeToJson();
         System.IO.File.WriteAllText(jsonPath, json);
         notificationSystem.FinishedExportingData();
