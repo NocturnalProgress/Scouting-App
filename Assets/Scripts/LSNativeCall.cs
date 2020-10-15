2e0f5b56638086a3271dc24edcda7f7ffa887d13
NocturnalProgress <26844071+NocturnalProgress@users.noreply.github.com>
2020-10-15 15:54:37 -0400

    Share menu testing

---
+++ Assets/Scripts/LSNativeCall.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+
+public class LSNativeCall
+{
+    [DllImport("__Internal")]
+    private static extern void UIActivityVC(TextAsset file);
+
+    public static void ShowUIActivityVC(TextAsset file)
+    {
+#if UNITY_IPHONE
+        UIActivityVC(file);
+#endif
+    }
+}
