53add7ae8a3fa882da51287bdf972bb0d02c5554
NocturnalProgress <26844071+NocturnalProgress@users.noreply.github.com>
2020-10-18 14:54:26 -0400

    Alternate scouting update

---
+++ Assets/Scripts/Alternate/Engine.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Engine : MonoBehaviour
+{
+    public void PowerPort(string color)
+    {
+        Debug.Log(color + " Power Port");
+    }
+
+    public void InitiationLine(string color)
+    {
+        Debug.Log(color + " Initiation Line");
+    }
+
+    public void Generator(string color)
+    {
+        Debug.Log(color + " Generator");
+
+
+    }
+}
