fa097a93f61fbb06527bf359451832c3e23e157f
NocturnalProgress <26844071+NocturnalProgress@users.noreply.github.com>
2020-10-19 19:36:05 -0400

    Added SendData notifications

--- Assets/Scripts/NotificationSystem.cs
+++ Assets/Scripts/NotificationSystem.cs
@@ -27,6 +27,15 @@ public class NotificationSystem : MonoBehaviour
 
         switch (index)
         {
+            case 6:
+                notification.GetComponent<Notification>().notificationMessage.text = "Error uploading form";
+                break;
+            case 5:
+                notification.GetComponent<Notification>().notificationMessage.text = "Form upload complete!";
+                break;
+            case 4: // Not connected to internet
+                notification.GetComponent<Notification>().notificationMessage.text = "Error: Not connected to internet";
+                break;
             case 3: // Finished saving scouting data
                 notification.GetComponent<Notification>().notificationMessage.text = "Finished saving scouting data!";
                 break;
@@ -41,6 +50,23 @@ public class NotificationSystem : MonoBehaviour
                 break;
         }
         index = 0;
+    }
+
+    public void ErrorUploadingForm()
+    {
+        index = 6;
+        DisplayNotificationCanvas();
+    }
+    public void FormUploadComplete()
+    {
+        index = 5;
+        DisplayNotificationCanvas();
+    }
+
+    public void NotConnectedToInternet()
+    {
+        index = 4;
+        DisplayNotificationCanvas();
     }
 
     public void FinishedSaving()
