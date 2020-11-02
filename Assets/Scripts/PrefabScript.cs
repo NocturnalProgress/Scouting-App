b4a219c549bd304a62ac9873a386239c3dbc798e
NocturnalProgress <26844071+NocturnalProgress@users.noreply.github.com>
2020-11-02 02:39:58 -0500

    Created prefab script for scouting data prefab

---
+++ Assets/Scripts/PrefabScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PrefabScript : MonoBehaviour
+{
+    public Button fileName;
+    public Button timeCreated;
+    public Button deleteButton;
+}
